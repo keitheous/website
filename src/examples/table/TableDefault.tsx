fe93c41cb082ff6a6912ccb12d95b837e66f1d8d
Ross Moody <fiftyfivehis@gmail.com>
2020-09-16 11:44:17 -0700

    update structure

--- src/examples/table/TableDefault.tsx
+++ src/examples/table/TableDefault.tsx
@@ -6,40 +6,37 @@
  */
 
 import React from 'react';
-import { Row as GRow, Col } from '@zendeskgarden/react-grid';
 import { Body, Cell, Head, HeaderCell, HeaderRow, Row, Table } from '@zendeskgarden/react-tables';
 
 const Example = () => (
-  <GRow style={{ overflow: 'auto' }}>
-    <Col style={{ minWidth: '500px' }}>
-      <Table>
-        <Head>
-          <HeaderRow>
-            <HeaderCell>Fruit type</HeaderCell>
-            <HeaderCell>Sun exposure</HeaderCell>
-            <HeaderCell>Soil type</HeaderCell>
-          </HeaderRow>
-        </Head>
-        <Body>
-          <Row>
-            <Cell>Raspberries</Cell>
-            <Cell>Partial shade</Cell>
-            <Cell>Moist and slightly acidic</Cell>
-          </Row>
-          <Row>
-            <Cell>Strawberries</Cell>
-            <Cell>Full sun</Cell>
-            <Cell>Medium moisture</Cell>
-          </Row>
-          <Row>
-            <Cell>Grapes</Cell>
-            <Cell>Full sun</Cell>
-            <Cell>Rich and well draining</Cell>
-          </Row>
-        </Body>
-      </Table>
-    </Col>
-  </GRow>
+  <div style={{ minWidth: '500px' }}>
+    <Table>
+      <Head>
+        <HeaderRow>
+          <HeaderCell>Fruit type</HeaderCell>
+          <HeaderCell>Sun exposure</HeaderCell>
+          <HeaderCell>Soil type</HeaderCell>
+        </HeaderRow>
+      </Head>
+      <Body>
+        <Row>
+          <Cell>Raspberries</Cell>
+          <Cell>Partial shade</Cell>
+          <Cell>Moist and slightly acidic</Cell>
+        </Row>
+        <Row>
+          <Cell>Strawberries</Cell>
+          <Cell>Full sun</Cell>
+          <Cell>Medium moisture</Cell>
+        </Row>
+        <Row>
+          <Cell>Grapes</Cell>
+          <Cell>Full sun</Cell>
+          <Cell>Rich and well draining</Cell>
+        </Row>
+      </Body>
+    </Table>
+  </div>
 );
 
 export default Example;
